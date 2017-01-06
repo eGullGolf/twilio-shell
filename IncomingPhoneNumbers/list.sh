4f8118245a6f8a94484734253331f1bb2402fb3e
Eric Bréchemier <eric@egull.co>
2017-01-06 15:12:23 +0100

    Add script to list all our incoming phone numbers
    Note: this resource is not accessible using test credentials.

---
+++ IncomingPhoneNumbers/list.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# List all messages (both inbound and outbound)
+#
+# Parameters:
+#   ... - see [1] for the list of optional parameters
+#
+# Reference:
+#   [1] Twilio REST API > REST API: Incoming Phone Numbers
+#       > List all IncomingPhoneNumber of your Account
+#   https://www.twilio.com/docs/api/rest/incoming-phone-numbers#list-get
+#
+cd "$(dirname "$0")"
+
+../request.sh GET IncomingPhoneNumbers $*
